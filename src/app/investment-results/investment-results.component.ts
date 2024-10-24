a42d5e6eedfc1595cdd53571d727755c72e9053d
Pritomprasantpaul <Pritomprasantpaul@gmail.com>
2024-10-24 18:24:23 +0530

    Added signal in service

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
@@ -13,8 +13,11 @@ export class InvestmentResultsComponent {
   // constructor( private investService : InvestmentService ) { }
   private investmentService = inject(InvestmentService);
 
-  get results() {
-    return this.investmentService.resultsData;
-  }
+  // get results() {
+  //   return this.investmentService.resultsData;
+  // }
   
+  // Both ways is correct
+  results = computed(() => this.investmentService.resultsData());
+  // results = this.investmentService.resultsData.asReadonly();
 }
